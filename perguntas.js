a2e01e5ef7ef1de933941595c906ea50da950f7e
Maju <169072510+majuzuuu@users.noreply.github.com>
2024-11-20 22:25:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Vi',
+    'Qual é o verdadeiro nome da Vi?',
+    'Violet'
+)
+
+criaCartao(
+    'Jinx',
+    'Qual é o parentesco da Jinx com a Vi?',
+    'São irmãs'
+)
+
+criaCartao(
+    'Ekko',
+    'Qual é o poder de Ekko?',
+    'Poder Temporal'
+)
+
+criaCartao(
+    'Arcane',
+    'Em que ano foi lançada a série?',
+    '2021'
+)
+
+criaCartao(
+    'Arcane',
+    'Qual é o par romântico de Vi?',
+    'Caitlyn'
+)
+
+criaCartao(
+    'Arcane',
+    'Em qual jogo é baseada a série?',
+    'LOL (League Of Legends)'
+)
